03e929f122a99c54746582ead194b6a51d41d33b
sjoertvv <sjoert.vanvelzen@gmail.com>
2019-05-07 10:59:44 -0400

    lb testing

--- testtime/time_ang_sep.py
+++ testtime/time_ang_sep.py
@@ -6,7 +6,32 @@ import time
 import numpy as np
 import sjoert.stellar
 from matplotlib import pyplot as plt
-import sjoert.starutil_numpy as sutil
+
+# we compare to pyspherematch's util.starutil_numpy
+import starutil_numpy as sutil
+
+ra, dec = 12, 0
+time0 = time.time()
+for i in range(1):
+    l, b = sutil.radectolb(ra, dec)
+print ('starutil', time.time() - time0)
+time0 = time.time()
+for i in range(1):
+    l, b = sjoert.stellar.radectolb(ra, dec)
+print ('astropy ', time.time() - time0)
+
+
+ra, dec = np.random.rand(100000)*300, 90-np.random.rand(100000)*90
+time0 = time.time()
+l, b = sutil.radectolb(ra, dec)
+print ('starutil', time.time()- time0)
+time0 = time.time()
+l1, b1 = sjoert.stellar.radectolb(ra, dec)
+print ('astropy ', time.time() -time0)
+print ('')
+print ('max difference in l/b', max(abs(l-l1)), max(abs(b-b1)))
+key = input('done.')
+
 
 def ang_sep_xyz(ra1, dec1, ra2, dec2):
     '''
@@ -69,30 +94,30 @@ dec2 =  np.random.rand(nn)*0.01-0.0001
 
 time0 = time.time()
 su_dist = sjoert.stellar.sutil.degrees_between(ra1, dec1, ra2[0], dec2[0])
-print 'time for starutil (ra1, dec1, ra2[0], dec2[0])', time.time() - time0
+print ('time for starutil (ra1, dec1, ra2[0], dec2[0])', time.time() - time0)
 
 time0 = time.time()
 su_dist2 = sjoert.stellar.sutil.degrees_between(ra1[0], dec1[0], ra2, dec2)
-print 'time for starutil (ra1[0], dec1[0], ra2, dec2)', time.time() - time0
+print ('time for starutil (ra1[0], dec1[0], ra2, dec2)', time.time() - time0)
 
 time0 = time.time()
 sj_dist = ang_sep_xyz(ra1, dec1, ra2[0], dec2[0])
-print 'time for ang_sep_xyz', time.time() - time0
+print ('time for ang_sep_xyz', time.time() - time0)
 
 time0 = time.time()
 ha_dist = sjoert.stellar.ang_sep(ra1, dec1, ra2[0], dec2[0])
-print 'time for Havesine', time.time() - time0
+print ('time for Havesine', time.time() - time0)
 
 time0 = time.time()
 sq_dist = np.sqrt( (ra1-ra2[0])**2 + (dec1-dec2[0])**2)
-print 'time for simple Euclid', time.time() - time0
+print ('time for simple Euclid', time.time() - time0)
 
 delta = su_dist-sj_dist
 #plt.clf()
-print 'mean, max difference between sjoert.angsep and starutil', np.mean(delta), np.max(np.abs(delta))
+print ('mean, max difference between sjoert.angsep and starutil', np.mean(delta), np.max(np.abs(delta)))
 
 delta = su_dist-ha_dist
-print 'mean, max difference between starutil and Havesin', np.mean(delta), np.max(np.abs(delta))
+print ('mean, max difference between starutil and Havesin', np.mean(delta), np.max(np.abs(delta)))
 #plt.hist(delta, bins=100)
 
 ii = np.argsort(np.random.rand(1000)*nn)
@@ -102,7 +127,7 @@ plt.scatter(ha_dist[ii],  sj_dist[ii], color='r', alpha=0.5)
 plt.plot(xx,xx)
 plt.xscale('log')
 plt.yscale('log')
-key = raw_input()
+key = input()
 
 plt.clf()
 plt.scatter(ha_dist[ii], ha_dist[ii] - sj_dist[ii], color='b', alpha=0.5)
